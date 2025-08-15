d9aea4066af82c827e5bdecbe2eab6d1be17fcea
rohith-0601 <rohithperugu3@gmail.com>
2025-08-15 17:23:01 +0530

    new things

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,20 +1,10 @@
 import './App.css';
-import { Button } from './components/button';
-import { Greet } from './components/Greet';
-import Heading from './components/heading';
-import { Oscar } from './components/oscar';
+
 
 function App() {
   return (
     <div className="App">
-      <Greet name='Rohith' somecount={6} />
-      <Heading>Hi this is Heading</Heading>
-      <Oscar>
-        <Heading>Oscar goes to me</Heading>
-      </Oscar>
-      <Button handleclick={(event)=>{
-        console.log("Button clicked",event);
-      }}></Button>
+      <h1>hello</h1>
     </div>
   );
 }
--- react-ts/src/components/oscar.tsx
+++
@@ -1,7 +0,0 @@
-type oscarprops = {
-    children : React.ReactNode
-}
-
-export const Oscar = (props:oscarprops) =>{
-    return <div><h2>{props.children}</h2></div>
-}
