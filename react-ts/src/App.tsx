f998500b87feedbb33d1dfc4acd53e4d752cc25f
rohith-0601 <rohithperugu3@gmail.com>
2025-08-18 16:12:23 +0530

    typescript practice

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>hello</h1>
-      <h2>lets learn typescript today</h2>
-      <h3>lets do after exam</h3>
+      <h1>lets start today night</h1>
     </div>
   );
 }
